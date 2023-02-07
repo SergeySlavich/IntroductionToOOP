4e9bbc3967530079e8d486ec1b33238263e5cf4a
Sergey <Kopcap_IV@mail.ru>
2023-02-07 19:57:01 +0500

    lection1

---
+++ string/main.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+using namespace std;
+
+class String
+{
+	int size;		//размер строки
+	char* str;		//адрес строки в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	//		Constructors:
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete this->str;
+		cout << "Destructor:\t" << this << endl;
+	}
+	//		Methods:
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "str:\t" << str << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	String str1(5);
+	str1.print();
+}
