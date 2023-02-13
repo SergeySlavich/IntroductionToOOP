251fad6fa4ea6e90d5d068e35da31e653578f1af
Sergey <Kopcap_IV@mail.ru>
2023-02-13 22:49:45 +0500

    SeparationDone

---
+++ Matrix/String.cpp
@@ -0,0 +1,110 @@
+﻿#include<iostream>
+#include"String.h"
+
+/////////////////////////////////////////////////////////////////
+/////////// Определение класса - CLASS DEFINITION  //////////////
+/////////////////////////////////////////////////////////////////
+
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//				Constructors:
+String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "DefConstructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "1ArgConstructor:" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//Deep copy - побитовое копирование
+	cout << "CopyConstructor:" << this << endl;
+}
+String::String(String&& other)noexcept :size(other.size), str(other.str)
+{
+	//Shallow copy - поверхностное копирование
+	other.size = 0;
+	other.str = nullptr;		//nullptr - это указатель на ноль.
+	cout << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete this->str;
+	cout << "Destructor:\t" << this << endl;
+}
+
+//				Operators:
+String& String::operator=(const String& other)
+{
+	//			l-value = r-value;
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)
+		this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//				Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+
+String operator+(const String& left, const String& right)
+{
+	String cat(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+		cat[i] = left[i];
+	//cat.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat[i + left.get_size() - 1] = right[i];
+	//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return cat;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+/////////////////////////////////////////////////////////////////
+/////////// Конец определения класса - CLASS DEFINITION END  ////
+/////////////////////////////////////////////////////////////////
---
+++ Matrix/String.h
@@ -0,0 +1,46 @@
+﻿//String.h
+#pragma once
+//StringSeparation
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+/////////////////////////////////////////////////////////////////
+/////////// Объявление класса - CLASS DECLARATION  //////////////
+/////////////////////////////////////////////////////////////////
+
+class String;
+String operator+(const String& left, const String& right);
+std::ostream& operator<<(std::ostream& os, const String& obj);
+
+class String
+{
+	int size;		//размер строки в Байтах
+	char* str;		//адрес строки в динамической памяти
+public:
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+
+	//				Constructors:
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other)noexcept;
+	~String();
+
+	//				Operators:
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
+	const char& operator[](int i)const;
+	char& operator[](int i);
+	//				Methods:
+	void print()const;
+};
+
+/////////////////////////////////////////////////////////////////
+/////////// Конец объявления класса - CLASS DECLARATION  ////////
+/////////////////////////////////////////////////////////////////
